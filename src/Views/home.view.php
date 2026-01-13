d40079f7f60eac6ec140430b8730ffeb493bf5b2
giovanne <giovannemmelo@gmail.com>
2026-01-13 10:46:40 -0300

    feat: adicionando logo e cabecalho

--- src/Views/home.view.php
+++ src/Views/home.view.php
@@ -1,4 +1,5 @@
 <?php require __DIR__ . '/templates/head.php'; ?>
+<?php require __DIR__ . '/templates/header.php'; ?>
 
 <h2>Hello world</h2>
 
---
+++ src/Views/templates/header.php
@@ -0,0 +1,13 @@
+<header class="headerCommon">
+    <img src="<?= base_link("public/images/logo.png") ?>" alt="Old motors" class="logo">
+    <nav>
+        <ul class="headerCommon__navbar">
+            <li>
+                <a href="#" class="headerCommon__link--active">Home</a>
+            </li>
+            <li>
+                <a href="#" class="headerCommon__link">Ver Carros</a>
+            </li>
+        </ul>
+    </nav>
+</header>
