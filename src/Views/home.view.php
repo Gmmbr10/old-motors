1c72d322bbf270831f6ca9eadafdc6ac28d80626
giovanne <giovannemmelo@gmail.com>
2026-01-13 11:21:06 -0300

    feat: adicionando o estilo do carrossel

--- src/Views/home.view.php
+++ src/Views/home.view.php
@@ -1,6 +1,10 @@
 <?php require __DIR__ . '/templates/head.php'; ?>
 <?php require __DIR__ . '/templates/header.php'; ?>
 
-<h2>Hello world</h2>
+<div class="carrossel">
+    <img
+        src="https://images.unsplash.com/photo-1459603677915-a62079ffd002?q=80&w=1534&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="">
+</div>
 
 <?php require __DIR__ . '/templates/footer.php'; ?>
