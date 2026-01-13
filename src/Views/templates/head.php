630ad22a0434f4a66215972b6cb74484373a126d
giovanne <giovannemmelo@gmail.com>
2026-01-13 10:47:03 -0300

    feat: iniciando estilo

--- src/Views/templates/head.php
+++ src/Views/templates/head.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Old Motors</title>
-    <link rel="stylesheet" href="<?= base_link("public/style/globals.css") ?>">
+    <link rel="stylesheet" href="<?= base_link("public/style/index.css") ?>">
 </head>
 
 <body>
