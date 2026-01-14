58cdc1cf37c91bc1742fe78d1a4ac4cf5e0f1cb4
giovanne <giovannemmelo@gmail.com>
2026-01-14 18:41:11 -0300

    feat: adicionando a classe de autenticacao

---
+++ src/Core/Authenticator.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Core;
+
+class Authenticator
+{
+    public function attempt(string $email, string $password): bool
+    {
+        $user = App::resolve('db')->query('SELECT * FROM users WHERE email = :email', [
+            'email' => $email,
+        ])->find();
+
+        if ($user && password_verify($password, $user['password'])) {
+            $this->login([
+                'id' => $user['id'],
+                'email' => $user['email'],
+                'role' => $user['type'],
+            ]);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public function login(array $user): void
+    {
+        $_SESSIOn['user'] = [
+            'id' => $user['id'],
+            'email' => $user['email'],
+        ];
+
+        session_regenerate_id(true);
+    }
+
+    public function logout(): void
+    {
+        Session::destroy();
+    }
+}
