5b72b04d5b562b6f32dfa40ec424b80f8f521e15
giovanne <giovannemmelo@gmail.com>
2026-01-14 16:15:15 -0300

    repair: adicionando o tamanho da senha na tabela users

--- sql/database.sql
+++ sql/database.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     fullname VARCHAR(100) NOT NULL,
     email VARCHAR(150) NOT NULL,
-    password VARCHAR() NOT NULL,
+    password VARCHAR(150) NOT NULL,
     type ENUM('common','admin','saleman') NOT NULL DEFAULT('common'),
     number VARCHAR(20),
     created_at DATETIME NOT NULL DEFAULT NOW(),
