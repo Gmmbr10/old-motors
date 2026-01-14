b9c2e906aaf78dcd5174a440a702322ef2f40956
giovanne <giovannemmelo@gmail.com>
2026-01-14 16:13:49 -0300

    feat: adicionando a tabela dos usuarios

--- sql/database.sql
+++ sql/database.sql
@@ -1,3 +1,14 @@
 CREATE DATABASE IF NOT EXISTS old_motors;
 
 USE old_motors;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    fullname VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL,
+    password VARCHAR() NOT NULL,
+    type ENUM('common','admin','saleman') NOT NULL DEFAULT('common'),
+    number VARCHAR(20),
+    created_at DATETIME NOT NULL DEFAULT NOW(),
+    updated_at DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW()
+);
