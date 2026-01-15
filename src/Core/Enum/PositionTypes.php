cabaae4b434200a6cb88ac7aad84821a89b1aede
giovanne <giovannemmelo@gmail.com>
2026-01-15 17:46:44 -0300

    feat: adicionando o enum de tipos de usuarios

---
+++ src/Core/Enum/PositionTypes.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Core\Enum;
+
+enum PositionTypes: string
+{
+    case Admin = 'admin';
+    case Common = 'common';
+    case Saleman = ' saleman';
+
+
+    public static function content(PositionTypes $type): string
+    {
+        return match ($type) {
+            self::Admin => 'Administrador',
+            self::Saleman => 'Vendedor',
+            self::Common => 'Comum',
+            default => 'Desconhecido'
+        };
+    }
+}
