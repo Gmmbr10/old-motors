b65081a040a75de3b37d527dde06be9ff9c0b983
giovanne <giovannemmelo@gmail.com>
2026-01-15 15:43:03 -0300

    repair: ajustando o tipo do retorno e lancando corretamente as excecoes

--- src/Core/ValidatorException.php
+++ src/Core/ValidatorException.php
@@ -8,13 +8,13 @@ class ValidatorException extends \Exception
     public readonly array $errors;
     public readonly array $old;
 
-    public static function throw(array $errors, array $old): static
+    public static function throw(array $errors, array $old): void
     {
         $instance = new static();
 
         $instance->errors = $errors;
         $instance->old = $old;
 
-        return $instance;
+        throw $instance;
     }
 }
