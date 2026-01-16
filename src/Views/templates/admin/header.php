c0830648feb2221db45b79039cbbd26303fa8f13
giovanne <giovannemmelo@gmail.com>
2026-01-16 17:55:33 -0300

    repair: atualizando o link dos funcionarios para aceitar quando estiver na pagina de editar

--- src/Views/templates/admin/header.php
+++ src/Views/templates/admin/header.php
@@ -7,7 +7,7 @@
                     <a href="<?= base_link("admin/home") ?>" class="<?= isUrl('admin/home') ? 'header__link--active' : 'header__link' ?>">Home</a>
                 </li>
                 <li class="dropdown">
-                    <p class="dropdown__btn <?= isUrl('admin/funcionarios') || isUrl('admin/funcionarios/cadastrar') ? 'header__link--active' : 'header__link' ?>">Funcionários</p>
+                    <p class="dropdown__btn <?= isUrl('admin/funcionarios') || isUrl('admin/funcionarios/cadastrar') || isUrl('admin/funcionarios/editar') ? 'header__link--active' : 'header__link' ?>">Funcionários</p>
                     <div class="dropdown--content">
                         <a href="<?= base_link("admin/funcionarios") ?>" class="<?= isUrl('admin/funcionarios') ? 'header__link--active' : 'header__link' ?>">Listar</a>
                         <a href="<?= base_link("admin/funcionarios/cadastrar") ?>" class="<?= isUrl('admin/funcionarios/cadastrar') ? 'header__link--active' : 'header__link' ?>">Cadastrar</a>
