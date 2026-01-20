add2fe92a3842016413709f418b21fde1e53e510
giovanne <giovannemmelo@gmail.com>
2026-01-20 17:41:09 -0300

    feat: criando a tabela de veiculos e imagem dos veiculos

--- sql/database.sql
+++ sql/database.sql
@@ -15,3 +15,24 @@ CREATE TABLE IF NOT EXISTS users (
 
 # user com senha 12345678
 INSERT INTO users (fullname, email, password, type) VALUES ('Admin', 'admin@oldmotors.com','$2y$12$xJufFosuLubKJ7/Op986sexObAcN7D9hUDAIsCx4GcwALioDdjKCm','admin');
+
+CREATE TABLE IF NOT EXISTS vehicles (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    mark VARCHAR(100) NOT NULL,
+    model VARCHAR(100) NOT NULL,
+    year VARCHAR(4) NOT NULL,
+    plate VARCHAR(10) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    status ENUM('sketch','completed') NOT NULL DEFAULT('sketch'),
+    created_at DATETIME NOT NULL DEFAULT NOW(),
+    updated_at DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW()
+);
+
+CREATE TABLE IF NOT EXISTS vehicle_images (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    vehicle_id INT,
+    path VARCHAR(255) NOT NULL,
+    main BOOLEAN DEFAULT 0,
+    created_at DATETIME NOT NULL DEFAULT NOW(),
+    FOREIGN KEY(vehicle_id) REFERENCES vehicles(id)
+);
