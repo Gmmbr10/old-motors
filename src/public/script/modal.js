b498694623fd167a3484173f014c435e7bc30032
giovanne <giovannemmelo@gmail.com>
2026-01-22 16:16:16 -0300

    feat: adicionando um tipo de openModal que envia formulario

--- src/public/script/modal.js
+++ src/public/script/modal.js
@@ -13,3 +13,22 @@ function closeModal(id)
     element.close();
     element.style.display = 'none';
 }
+
+function openModalForm(id, formId = null)
+{
+    const element = document.getElementById(id);
+    
+    element.showModal();
+    element.style.display = 'flex';
+
+    if (formId == null) {
+        return;
+    }
+
+    const button = element.getElementsByTagName('button')[0];
+
+    button.addEventListener('click', () => {
+        const form = document.getElementById(formId);
+        form.submit();
+    });
+}
